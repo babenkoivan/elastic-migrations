e36c148ac2877c6e1de683211873612d22df2fd1
ivanbabenko <babenko.i.a@gmail.com>
2020-12-07 09:36:32 +0100

    Use SCOUT_PREFIX as a fallback for index name prefix, disable xdebug in CI

--- config/elastic.migrations.php
+++ config/elastic.migrations.php
@@ -3,5 +3,5 @@
 return [
     'table' => env('ELASTIC_MIGRATIONS_TABLE', 'elastic_migrations'),
     'storage_directory' => env('ELASTIC_MIGRATIONS_DIRECTORY', base_path('elastic/migrations')),
-    'index_name_prefix' => env('ELASTIC_MIGRATIONS_INDEX_NAME_PREFIX', ''),
+    'index_name_prefix' => env('ELASTIC_MIGRATIONS_INDEX_NAME_PREFIX', env('SCOUT_PREFIX', '')),
 ];
