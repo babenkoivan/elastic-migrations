6d5ef520c49c644bf9c90f60a246edfe2f5de597
Steve Bauman <steven_bauman@outlook.com>
2021-05-27 13:44:24 -0400

    Fix signature

--- src/Console/FreshCommand.php
+++ src/Console/FreshCommand.php
@@ -14,7 +14,7 @@ class FreshCommand extends Command
     /**
      * @var string
      */
-    protected $signature = 'elastic:fresh 
+    protected $signature = 'elastic:migrate:fresh 
         {--force : Force the operation to run when in production}';
 
     /**
